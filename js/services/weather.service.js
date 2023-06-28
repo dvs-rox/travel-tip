ae123e41ffc26f05db8500266569a671d4ae6f24
Dore Zahavy <dorezahavy@gmail.com>
2023-06-28 18:40:37 +0300

    copylink

--- js/services/weather.service.js
+++ js/services/weather.service.js
@@ -2,4 +2,11 @@
 
 const API_KEY = '0cacfaecd8ce2baa63d434978d889723'
 
-// 'http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${W_KEY'
+
+
+function getWeather(place){
+    const url = `http://api.openweathermap.org/data/2.5/weather?lat=${place.lat}&lon=${place.lng}&APPID=${API_KEY}`
+
+    return axios.get(url).then(res=>res.data)
+
+}
