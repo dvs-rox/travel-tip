25edfa3b5517fa91bd1bf4743152b517e9a8b4d3
Dore Zahavy <dorezahavy@gmail.com>
2023-06-28 18:19:14 +0300

    more commits

--- js/controllers/app.controller.js
+++ js/controllers/app.controller.js
@@ -6,6 +6,7 @@ window.onAddPlace = onAddPlace
 window.onPanTo = onPanTo
 window.onGetLocs = onGetLocs
 window.onGetUserPos = onGetUserPos
+window.onRemovePlace = onRemovePlace
 
 function onInit() {
     mapService.initMap()
@@ -13,9 +14,6 @@ function onInit() {
             console.log('Map is ready')
         })
         .catch(() => console.log('Error: cannot init map'))
-
-    placeService.getPlaces()
-        .then(renderPlacesList)
 }
 
 // This function provides a Promise API to the callback-based-api of getCurrentPosition
@@ -28,17 +26,22 @@ function getPosition() {
 function onAddPlace() {
     console.log('Adding a place')
     // mapService.addMarker({ lat: 32.0749831, lng: 34.9120554 })
-    console.log(window.currLatLng)
-    placeService.addPlace(window.currLatLng, 'puki')
+    placeService.addPlace(window.currLatLng,'puki')
+    renderPlacesList()
 }
 
-// function onGetLocs() {
-//     locService.getLocs()
-//         .then(locs => {
-//             console.log('Locations:', locs)
-//             document.querySelector('.locs').innerText = JSON.stringify(locs, null, 2)
-//         })
-// }
+function onRemovePlace(placeId){
+    removePlace(placeId)
+    renderPlacesList()
+}
+
+function onGetLocs() {
+    locService.getLocs()
+        .then(locs => {
+            console.log('Locations:', locs)
+            document.querySelector('.locs').innerText = JSON.stringify(locs, null, 2)
+        })
+}
 
 function onGetUserPos() {
     getPosition()
@@ -46,7 +49,7 @@ function onGetUserPos() {
             console.log('User position is:', pos.coords)
             document.querySelector('.user-pos').innerText =
                 `Latitude: ${pos.coords.latitude} - Longitude: ${pos.coords.longitude}`
-            //TODO: pan map to location
+                //TODO: pan map to location
         })
         .catch(err => {
             console.log('err!!!', err)
@@ -62,12 +65,12 @@ function onPanTo(pos) {
 
 //PLACES STUFF AREA
 
-function renderPlacesList(places) {
-    const strHTMLs = places.map(place => {
+function renderPlacesList(places){
+   const strHTMLs = places.map(place=>{
         return `
         <li>
         <h4>${place.name}<h4>
-        <button onclick="onPanTo(${{ lat: place.lat, lng: place.lng }})">Go</button>
+        <button onclick="onPanTo(${{lat:place.lat,lng:place.lng}})">Go</button>
         <button onclick="onRemovePlace(${place.id})">Delete</button>
         </li>
         `
